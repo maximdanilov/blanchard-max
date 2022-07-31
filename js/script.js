f73745e76d93287f1348245f5f28c8328eb09f6d
Admin <magisterfizic@mail.ru>
2022-07-31 14:44:57 +1200

    add fonts and more

--- js/script.js
+++ js/script.js
@@ -254,7 +254,6 @@ document.addEventListener("DOMContentLoaded", function () {
     // contacts
     //form
     var selector = document.querySelector("input[type='tel']");
-
     var im = new Inputmask("+7 (999) 999-99-99");
     im.mask(selector);
 
@@ -330,5 +329,4 @@ $(document).ready(function() {
 		});
 		return false;
 	});
-
 });
