235b4a53a2ddded9120f4e4f54053b425c2d463a
Admin <magisterfizic@mail.ru>
2022-08-16 22:00:32 +1200

    исправил

--- js/script.js
+++ js/script.js
@@ -1,305 +1,314 @@
-document.addEventListener("DOMContentLoaded", function () {
-    // header
-    // burger
+// burger events
+let burger = document.querySelector('.header__burger');
+let menu = document.querySelector('.header__nav');
+let menuLinks = document.querySelectorAll('.close-link');
+let topHeader = document.querySelector('.header__top');
 
-    let burger = document.querySelector('.header__burger');
-    let menu = document.querySelector('.header__nav');
-    let menuLinks = document.querySelectorAll('.close-link');
-    let topHeader = document.querySelector('.header__top');
+burger.addEventListener('click', function (){
+  document.body.classList.toggle('stop-scroll');
+  burger.classList.toggle('header__burger--active');
+  burger.setAttribute("aria-expanded", "true");
+  menu.classList.toggle('header__nav--active');
+  topHeader.classList.toggle('header-top--active');
+})
 
-    burger.addEventListener('click', function (){
-      document.body.classList.toggle('stop-scroll');
-      burger.classList.toggle('header__burger--active');
-      burger.setAttribute("aria-expanded", "true");
-      menu.classList.toggle('nav--active');
-      topHeader.classList.toggle('header__top--active');
-    })
+menuLinks.forEach(function (el) {
+  el.addEventListener('click', function () {
+    document.body.classList.remove('stop-scroll');
+    burger.classList.remove('header__burger--active');
+    burger.setAttribute("aria-expanded", "false");
+    menu.classList.remove('header__nav--active');
+    topHeader.classList.remove('header__top--active');
+  })
+})
 
-    menuLinks.forEach(function (el) {
-      el.addEventListener('click', function () {
-        document.body.classList.remove('stop-scroll');
-        burger.classList.remove('header__burger--active');
-        burger.setAttribute("aria-expanded", "false");
-        menu.classList.remove('nav--active');
-        topHeader.classList.remove('header__top--active');
-      })
-    })
+// header-top search
+let openBtn = document.querySelector('.header__search-mobile-open');
+let closeBtn = document.querySelector('.header__form-mobile-close');
+let inputSearch = document.querySelector('.header__form-mobile');
 
-    // search
+openBtn.addEventListener('click', function () {
+  inputSearch.classList.add('header__form-mobile--active');
+});
 
-    const openSearchHandler = () => {
-        document.querySelector('#search-open').classList.toggle('search-active');
-        document.querySelector('#search-btn').classList.toggle('search-active');
-    };
+closeBtn.addEventListener('click', function () {
+  inputSearch.classList.remove('header__form-mobile--active');
+})
 
-    document.querySelector('#search-btn').addEventListener('click', function () {
-        openSearchHandler();
-    });
-    document.querySelector('#search-close').addEventListener('click', function () {
-        openSearchHandler();
-    });
+// dropdown scroll
+const bars = document.querySelectorAll('[data-simplebar]')
 
-    // tab
+bars.forEach(el => {
+  new SimpleBar(el, {
+    ariaLabel: 'Прокручиваемая область',
+  });
+  el.querySelector('.simplebar-content-wrapper').setAttribute('tabindex', '-1');
+});
 
-    document.querySelectorAll(".header__btn-tab").forEach(item => {
-        item.addEventListener("click", function () {
-            let btn = this;
-            let dropdown = this.parentElement.querySelector(".header__tab-dropdown");
+// header-bottom dropdown
+const menuBtn = document.querySelectorAll('.header__btn-tab');
+const drops = document.querySelectorAll('.header__tab-dropdown');
 
-            document.querySelectorAll(".header__btn-tab").forEach(el => {
-                if (el !== btn) {
-                    el.classList.remove("active-btn");
-                }
-            });
+menuBtn.forEach(function (btn) {
+  btn.addEventListener('click', function (e) {
+    let currentBtn = e.currentTarget;
+    let currentDrop = currentBtn.closest('.header__bottom-menu-item').querySelector('.header__tab-dropdown');
 
-            document.querySelectorAll(".header__tab-dropdown").forEach(el => {
-                if (el !== dropdown) {
-                    el.classList.remove("active-dropdown");
-                }
-            })
-            dropdown.classList.toggle("active-dropdown");
-            btn.classList.toggle("active-btn")
-        })
-
-
-        document.addEventListener("click", function (e) {
-            let target = e.target;
-            if (!target.closest(".header__bottom-list")) {
-                document.querySelectorAll(".header__tab-dropdown").forEach(el => {
-                    el.classList.remove("active-dropdown");
-                })
-                document.querySelectorAll(".header__btn-tab").forEach(el => {
-                    el.classList.remove("active-btn");
-                });
-            }
-        })
-    });
-   
-
-    //scroll
-    new SimpleBar(document.querySelector(".simplebar"), {
-        autoHide: false,
+    menuBtn.forEach(function (el) {
+      if (el !== currentBtn) {
+        el.classList.remove('header__btn-tab--active');
+        el.setAttribute("aria-expanded", "false");
+      }
     });
 
-    // gallery
-    // select
-    const elemenGallery = document.querySelector('#gallery__select');
-    const choiceGallery = new Choices(elemenGallery, {
-        searchEnabled: false,
-        // position: 'bottom'
+    drops.forEach(function (el) {
+      if (el !== currentDrop) {
+        el.classList.remove('header__tab-dropdown--active');
+        el.setAttribute("aria-hidden", "true");
+      }
     });
 
-    // swiper
-    const swiper = new Swiper('.gallery__swiper-container', {
-        slidesPerView: 1,
-        slidesPerGroup: 1,
-        // spaceBetween: 50,
-        loop: false,
-        navigation: {
-            nextEl: '.gallery__swiper-button-next',
-            prevEl: '.gallery__swiper-button-prev',
+    currentDrop.classList.toggle('header__tab-dropdown--active');
+    currentDrop.setAttribute("aria-hidden", "false");
+    currentBtn.classList.toggle('header__btn-tab--active');
+    currentBtn.setAttribute("aria-expanded", "true");
+  });
+});
+
+document.addEventListener('click', function (e) {
+  if (!e.target.closest('.header__bottom-menu-list')) {
+
+    menuBtn.forEach(function (el) {
+      el.classList.remove('header__btn-tab--active');
+      el.setAttribute("aria-expanded", "false");
+    });
+
+    drops.forEach(function (el) {
+      el.classList.remove('header__tab-dropdown--active');
+      el.setAttribute("aria-hidden", "true");
+    });
+  }
+});
+
+// gallery
+// select
+const elemenGallery = document.querySelector('#gallery__select');
+const choiceGallery = new Choices(elemenGallery, {
+    searchEnabled: false,
+    // position: 'bottom'
+});
+
+// swiper
+const swiper = new Swiper('.gallery__swiper-container', {
+    slidesPerView: 1,
+    slidesPerGroup: 1,
+    // spaceBetween: 50,
+    loop: false,
+    navigation: {
+        nextEl: '.gallery__swiper-button-next',
+        prevEl: '.gallery__swiper-button-prev',
+    },
+    pagination: {
+        el: '.gallery__swiper-pagination',
+        type: 'fraction',
+    },
+    breakpoints: {
+        480: {
+            slidesPerView: 2,
+            spaceBetween: 38,
+            slidesPerGroup: 2,
         },
-        pagination: {
-            el: '.gallery__swiper-pagination',
-            type: 'fraction',
+        1008: {
+            slidesPerView: 2,
+            spaceBetween: 34,
+            slidesPerGroup: 2,
         },
-        breakpoints: {
-            480: {
-                slidesPerView: 2,
-                spaceBetween: 38,
-                slidesPerGroup: 2,
-            },
-            1008: {
-                slidesPerView: 2,
-                spaceBetween: 34,
-                slidesPerGroup: 2,
-            },
-            1760: {
-                slidesPerView: 3,
-                spaceBetween: 50,
-                slidesPerGroup: 3,
-            }
+        1760: {
+            slidesPerView: 3,
+            spaceBetween: 50,
+            slidesPerGroup: 3,
         }
-    });
-
-    // events
-    // swiper
-    const swiperEvents = new Swiper('.events__swiper-container', {
-        slidesPerView: 1,
-        slidesPerGroup: 1,
-        spaceBetween: 40,
-        loop: false,
-        navigation: {
-            nextEl: '.events__swiper-button-next',
-            prevEl: '.events__swiper-button-prev',
-        },
-        pagination: {
-            el: '.events__swiper-pagination',
-            clickable: true
-        },
-        breakpoints: {
-            687: {
-                slidesPerView: 2,
-                spaceBetween: 34,
-                slidesPerGroup: 2,
-            },
-            1000: {
-                slidesPerView: 3,
-                spaceBetween: 27,
-                slidesPerGroup: 3,
-            },
-            1761: {
-                slidesPerView: 3,
-                spaceBetween: 50,
-                slidesPerGroup: 3,
-            },
-        }
-    });
-    
-    // catalog
-    // accordion
-    $(".accordion").accordion({
-        heightStyle: "content",
-        active: false,
-        collapsible: true,
-        animate: 500
-    });
-    document.querySelectorAll('.catalog__item-top').forEach(function (el) {
-        el.addEventListener('mousedown', e => e.preventDefault());
-    });
-
-    // tab
-    document.querySelectorAll('.catalog__bottom-btn').forEach(function (tabsBtn) {
-        tabsBtn.addEventListener('click', function (e) {
-            const path = e.currentTarget.dataset.path;
-            document.querySelectorAll('.catalog__bottom-btn').forEach(function (btn) {
-                btn.classList.remove('catalog__bottom-btn--active')
-            });
-            e.currentTarget.classList.add('catalog__bottom-btn--active');
-            document.querySelectorAll('.catalog__content-info').forEach(function (tabsBtn) {
-                tabsBtn.classList.remove('catalog__content-info--active')
-            });
-            document.querySelector(`[data-target="${path}"]`).classList.add('catalog__content-info--active');
-        });
-    });
-
-    // projects
-    // swiper
-    const swiperProjects = new Swiper('.projects__swiper-container', {
-        slidesPerView: 1,
-        slidesPerGroup: 1,
-        // spaceBetween: 50,
-        loop: false,
-        navigation: {
-            nextEl: '.projects__swiper-button-next',
-            prevEl: '.projects__swiper-button-prev',
-        },
-        breakpoints: {
-            500: {
-                slidesPerView: 2,
-                spaceBetween: 33.97,
-                slidesPerGroup: 2,
-            },
-            1008: {
-                slidesPerView: 2,
-                spaceBetween: 50,
-                slidesPerGroup: 2,
-            },
-            1750: {
-                slidesPerView: 3,
-                spaceBetween: 50,
-                slidesPerGroup: 3,
-            }
-        },
-    })
-
-    // map
-    ymaps.ready(init);
-    function init() {
-        var myMap = new ymaps.Map("map", {
-            center: [55.760109, 37.591952],
-            zoom: 14,
-            controls: [],
-        });
-        var myPlacemark = new ymaps.Placemark([55.760109, 37.591952], {}, {
-            iconLayout: 'default#image',
-            iconImageHref: 'img/placemarker.svg',
-            iconImageSize: [20, 20],
-            iconImageOffset: [-2, -40],
-        });
-
-        myMap.controls.add('zoomControl', {
-            float: 'none',
-            position: {
-                right: 11,
-                top: 300
-            }
-        }).add('geolocationControl', {
-            float: 'none',
-            position: {
-                right: 11,
-                top: 380
-            }
-        });
-        myMap.geoObjects.add(myPlacemark);
     }
+});
 
-    // contacts
-    //form
-    let selector = document.querySelector("input[type='tel']");
-    let im = new Inputmask("+7 (999) 999-99-99");
-    im.mask(selector);
+// events
+// swiper
+const swiperEvents = new Swiper('.events__swiper-container', {
+    slidesPerView: 1,
+    slidesPerGroup: 1,
+    spaceBetween: 40,
+    loop: false,
+    navigation: {
+        nextEl: '.events__swiper-button-next',
+        prevEl: '.events__swiper-button-prev',
+    },
+    pagination: {
+        el: '.events__swiper-pagination',
+        clickable: true
+    },
+    breakpoints: {
+        687: {
+            slidesPerView: 2,
+            spaceBetween: 34,
+            slidesPerGroup: 2,
+        },
+        1000: {
+            slidesPerView: 3,
+            spaceBetween: 27,
+            slidesPerGroup: 3,
+        },
+        1761: {
+            slidesPerView: 3,
+            spaceBetween: 50,
+            slidesPerGroup: 3,
+        },
+    }
+});
+    
+// catalog
+// accordion
+$(".accordion").accordion({
+    heightStyle: "content",
+    active: false,
+    collapsible: true,
+    animate: 500
+});
+document.querySelectorAll('.catalog__item-top').forEach(function (el) {
+    el.addEventListener('mousedown', e => e.preventDefault());
+});
 
-    new JustValidate('.contacts__form', {
-        rules: {
-            name: {
-                required: true,
-                minLength: 2,
-                maxLength: 30
-            },
-            tel: {
-                required: true,
-                function: (name, value) => {
-                    const phone = selector.inputmask.unmaskedvalue()
-                    return Number(phone) && phone.length === 10
-                }
-            }
-        },
-        messages: {
-            name: {
-                required: "Как вас зовут?",
-                minLength: "Недопустимый формат",
-            },
-            tel: {
-                required: "Укажите ваш телефон",
-                function: "Недопустимый формат",
-            },
-        }
-    });
-    new JustValidate('.contacts__request-form', {
-        rules: {
-            name: {
-                required: true,
-                minLength: 2,
-                maxLength: 30
-            },
-            mail: {
-                required: true,
-                email: true
-            }
-        },
-        messages: {
-            name: {
-                required: "Как вас зовут?",
-                minLength: "Недопустимый формат",
-            },
-            mail: {
-                required: "Укажите ваш email",
-                function: "Недопустимый формат",
-            }
-        }
+// tab
+document.querySelectorAll('.catalog__bottom-btn').forEach(function (tabsBtn) {
+    tabsBtn.addEventListener('click', function (e) {
+        const path = e.currentTarget.dataset.path;
+        document.querySelectorAll('.catalog__bottom-btn').forEach(function (btn) {
+            btn.classList.remove('catalog__bottom-btn--active')
+        });
+        e.currentTarget.classList.add('catalog__bottom-btn--active');
+        document.querySelectorAll('.catalog__content-info').forEach(function (tabsBtn) {
+            tabsBtn.classList.remove('catalog__content-info--active')
+        });
+        document.querySelector(`[data-target="${path}"]`).classList.add('catalog__content-info--active');
     });
 });
+
+// projects
+// swiper
+const swiperProjects = new Swiper('.projects__swiper-container', {
+    slidesPerView: 1,
+    slidesPerGroup: 1,
+    // spaceBetween: 50,
+    loop: false,
+    navigation: {
+        nextEl: '.projects__swiper-button-next',
+        prevEl: '.projects__swiper-button-prev',
+    },
+    breakpoints: {
+        500: {
+            slidesPerView: 2,
+            spaceBetween: 33.97,
+            slidesPerGroup: 2,
+        },
+        1008: {
+            slidesPerView: 2,
+            spaceBetween: 50,
+            slidesPerGroup: 2,
+        },
+        1750: {
+            slidesPerView: 3,
+            spaceBetween: 50,
+            slidesPerGroup: 3,
+        }
+    },
+})
+
+// map
+ymaps.ready(init);
+function init() {
+    var myMap = new ymaps.Map("map", {
+        center: [55.760109, 37.591952],
+        zoom: 14,
+        controls: [],
+    });
+    var myPlacemark = new ymaps.Placemark([55.760109, 37.591952], {}, {
+        iconLayout: 'default#image',
+        iconImageHref: 'img/placemarker.svg',
+        iconImageSize: [20, 20],
+        iconImageOffset: [-2, -40],
+    });
+
+    myMap.controls.add('zoomControl', {
+        float: 'none',
+        position: {
+            right: 11,
+            top: 300
+        }
+    }).add('geolocationControl', {
+        float: 'none',
+        position: {
+            right: 11,
+            top: 380
+        }
+    });
+    myMap.geoObjects.add(myPlacemark);
+}
+
+// contacts
+//form
+let selector = document.querySelector("input[type='tel']");
+let im = new Inputmask("+7 (999) 999-99-99");
+im.mask(selector);
+
+new JustValidate('.contacts__form', {
+    rules: {
+        name: {
+            required: true,
+            minLength: 2,
+            maxLength: 30
+        },
+        tel: {
+            required: true,
+            function: (name, value) => {
+                const phone = selector.inputmask.unmaskedvalue()
+                return Number(phone) && phone.length === 10
+            }
+        }
+    },
+    messages: {
+        name: {
+            required: "Как вас зовут?",
+            minLength: "Недопустимый формат",
+        },
+        tel: {
+            required: "Укажите ваш телефон",
+            function: "Недопустимый формат",
+        },
+    }
+});
+new JustValidate('.contacts__request-form', {
+    rules: {
+        name: {
+            required: true,
+            minLength: 2,
+            maxLength: 30
+        },
+        mail: {
+            required: true,
+            email: true
+        }
+    },
+    messages: {
+        name: {
+            required: "Как вас зовут?",
+            minLength: "Недопустимый формат",
+        },
+        mail: {
+            required: "Укажите ваш email",
+            function: "Недопустимый формат",
+        }
+    }
+});
+
 
 
 $(".modal").dialog({
